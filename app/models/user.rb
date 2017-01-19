28996d14745b2fc2b7b3e597f550b2812c1fd137
kumar <mukpdev11@gmail.com>
2017-01-19 19:31:26 +0530

    initial commet for login via twitter, google is done

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+     :omniauthable, :omniauth_providers => [:twitter, :facebook, :google_oauth2, :yahoo_oauth2]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.uid.to_s+'@twitter.com'
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+end
