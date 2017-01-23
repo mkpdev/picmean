e9f8a857848578482da6d589acd25a5930c605dc
kumar <mukpdev11@gmail.com>
2017-01-23 17:02:09 +0530

    commit for user email id from social login

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
+      auth.provider.eql?"twitter" ? user.email = auth.info.email : user.email = auth.email
       user.uid = auth.uid
-      user.email = auth.uid.to_s+'#{SecureRandom.hex(2)}@twitter.com'
       user.password = Devise.friendly_token[0,20]
     end
   end
