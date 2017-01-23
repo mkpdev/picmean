74ee41db89a94ac23ff58a7763ca9a9faeab5e3d
kumar <mukpdev11@gmail.com>
2017-01-23 19:53:15 +0530

    for user model unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,11 @@ class User < ApplicationRecord
      :omniauthable, :omniauth_providers => [:twitter, :facebook, :google_oauth2, :yahoo_oauth2]
 
   def self.from_omniauth(auth)
-    puts "#{auth}"
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
-      auth.provider.eql?"twitter" ? user.email = auth.info.email : user.email = auth.email
       user.uid = auth.uid
+      user.email = auth.uid.to_s+"#{SecureRandom.hex(2)}@twitter.com"
       user.password = Devise.friendly_token[0,20]
     end
-  end
-  def email_required?
-    false
   end
 end
